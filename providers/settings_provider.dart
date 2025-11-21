8f2d9324b4eb0638535702c0c90e08d94435a8b3
Mwende_Kyalo <105672753+Mwende-Kyalo@users.noreply.github.com>
2025-11-21 15:03:54 +0300

    Providers

---
+++ providers/settings_provider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsProvider with ChangeNotifier {
+  bool _isDarkMode = false;
+  double _fontSize = 14.0;
+  static const String _darkModeKey = 'dark_mode';
+  static const String _fontSizeKey = 'font_size';
+
+  bool get isDarkMode => _isDarkMode;
+  double get fontSize => _fontSize;
+
+  SettingsProvider() {
+    _loadSettings();
+  }
+
+  Future<void> _loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(_darkModeKey) ?? false;
+    _fontSize = prefs.getDouble(_fontSizeKey) ?? 14.0;
+    notifyListeners();
+  }
+
+  Future<void> toggleDarkMode() async {
+    _isDarkMode = !_isDarkMode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_darkModeKey, _isDarkMode);
+    notifyListeners();
+  }
+
+  Future<void> setFontSize(double size) async {
+    _fontSize = size;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble(_fontSizeKey, _fontSize);
+    notifyListeners();
+  }
+
+  Future<void> setDarkMode(bool value) async {
+    _isDarkMode = value;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_darkModeKey, _isDarkMode);
+    notifyListeners();
+  }
+}
+
